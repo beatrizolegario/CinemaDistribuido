c9c6584a0ece3435022571fe1a6399d9c4842078
Cauli Vilela <88348221+coquizin@users.noreply.github.com>
2025-08-13 10:19:50 -0300

    Update cinema.bat

---
+++ cinema.bat
@@ -0,0 +1,29 @@
+@echo off
+
+REM Define o diretório do projeto e as bibliotecas do ZooKeeper
+set PROJECT_DIR=src\main\java
+set ZK_DIR="C:\temp\apache-zookeeper-3.9.3-bin"
+
+REM Define o classpath com todas as bibliotecas necessárias
+set CP_ZK=.;%ZK_DIR%\lib\zookeeper-3.9.3.jar;%ZK_DIR%\lib\zookeeper-jute-3.9.3.jar;%ZK_DIR%\lib\slf4j-api-1.7.30.jar;%ZK_DIR%\lib\logback-core-1.2.13.jar;%ZK_DIR%\lib\logback-classic-1.2.13.jar;%ZK_DIR%\lib\netty-handler-4.1.113.Final.jar
+
+REM Adiciona o diretório do seu código fonte ao classpath para que o javac o encontre
+set CLASSPATH=%CP_ZK%;%PROJECT_DIR%
+
+echo Compilando CinemaTicketingSystem.java...
+javac -cp "%CLASSPATH%" %PROJECT_DIR%\CinemaTicketingSystem.java
+
+REM Verifica se a compilação foi bem-sucedida
+if %errorlevel% neq 0 (
+    echo A compilação falhou. Verifique os erros acima.
+    pause
+    exit /b 1
+)
+
+echo Compilação concluída com sucesso!
+echo Executando o programa...
+
+REM Executa a classe principal
+java -cp "%CLASSPATH%" CinemaTicketingSystem
+
+pause
